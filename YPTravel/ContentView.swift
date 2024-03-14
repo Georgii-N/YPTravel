c68241689059c4bb9e47267d11cecf7050b283c8
Georgii <neguritsageorgiy@gmail.com>
2024-03-14 12:30:51 +0400

    Initial commit

---
+++ YPTravel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YPTravel
+//
+//  Created by Georgiy Neguritsa on 14/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YPTravel/YPTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  YPTravelApp.swift
+//  YPTravel
+//
+//  Created by Georgiy Neguritsa on 14/3/24.
+//
+
+import SwiftUI
+
+@main
+struct YPTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
