fb6d0dbe49260ea7888343945b4aec00c5a34668
Georgii <neguritsageorgiy@gmail.com>
2024-03-15 18:05:53 +0400

    Добавил  клиент для Расписание рейсов между станциями

--- YPTravel/NearestStationsService.swift
+++ YPTravel/NearestStationsService.swift
@@ -4,6 +4,7 @@ import OpenAPIURLSession
 
 // 2. Улучшаем читаемость кода — необязательный шаг
 typealias NearestStations = Components.Schemas.Stations
+typealias TripsSchedule = Components.Schemas.TripsSchedule
 
 protocol NearestStationsServiceProtocol {
   func getNearestStations(lat: Double, lng: Double, distance: Int) async throws -> NearestStations
@@ -29,6 +30,18 @@ final class NearestStationsService: NearestStationsServiceProtocol {
     ))
     return try response.ok.body.json
   }
+    
+    func getTripsSchedule(_ from: String, _ to: String) async throws -> TripsSchedule {
+        let response = try await client.getTripsSchedule(
+            query: .init(
+                apikey: apikey,
+                from: from,
+                to: to
+            )
+        )
+        
+        return try response.ok.body.json
+    }
 }
 
 
@@ -46,5 +59,22 @@ func stations() {
     Task {
         let stations = try await service.getNearestStations(lat: 59.864177, lng: 30.319163, distance: 50)
         print(stations)
+    }
+}
+
+func schedule() {
+    let client = Client(
+        serverURL: try! Servers.server1(),
+        transport: URLSessionTransport()
+    )
+    
+    let service = NearestStationsService(
+        client: client,
+        apikey: "d52e2ffc-ec87-4d27-8247-5c7f412d514e"
+    )
+    
+    Task {
+        let schedule = try await service.getTripsSchedule("c146", "c213")
+        print(schedule)
     }
 }
