297298099fbf729030faeb2cb879c3bf1b1d7d20
Georgii <neguritsageorgiy@gmail.com>
2024-03-15 18:59:46 +0400

    Добавил клиент для "Расписание рейсов по станции"

--- YPTravel/NearestStationsService.swift
+++ YPTravel/NearestStationsService.swift
@@ -5,31 +5,32 @@ import OpenAPIURLSession
 // 2. Улучшаем читаемость кода — необязательный шаг
 typealias NearestStations = Components.Schemas.Stations
 typealias TripsSchedule = Components.Schemas.TripsSchedule
+typealias TripsByStation = Components.Schemas.TripsByStation
 
 protocol NearestStationsServiceProtocol {
-  func getNearestStations(lat: Double, lng: Double, distance: Int) async throws -> NearestStations
+    func getNearestStations(lat: Double, lng: Double, distance: Int) async throws -> NearestStations
 }
 
 final class NearestStationsService: NearestStationsServiceProtocol {
-  private let client: Client
-  private let apikey: String
-  
-  init(client: Client, apikey: String) {
-    self.client = client
-    self.apikey = apikey
-  }
-  
-  func getNearestStations(lat: Double, lng: Double, distance: Int) async throws -> NearestStations {
-  // В документе с описанием запроса мы задали параметры apikey, lat, lng и distance
-  // Для вызова сгенерированной функции нужно передать эти параметры
-    let response = try await client.getNearestStations(query: .init(
-        apikey: apikey,
-        lat: lat,
-        lng: lng,
-        distance: distance
-    ))
-    return try response.ok.body.json
-  }
+    private let client: Client
+    private let apikey: String
+    
+    init(client: Client, apikey: String) {
+        self.client = client
+        self.apikey = apikey
+    }
+    
+    func getNearestStations(lat: Double, lng: Double, distance: Int) async throws -> NearestStations {
+        // В документе с описанием запроса мы задали параметры apikey, lat, lng и distance
+        // Для вызова сгенерированной функции нужно передать эти параметры
+        let response = try await client.getNearestStations(query: .init(
+            apikey: apikey,
+            lat: lat,
+            lng: lng,
+            distance: distance
+        ))
+        return try response.ok.body.json
+    }
     
     func getTripsSchedule(_ from: String, _ to: String) async throws -> TripsSchedule {
         let response = try await client.getTripsSchedule(
@@ -42,8 +43,18 @@ final class NearestStationsService: NearestStationsServiceProtocol {
         
         return try response.ok.body.json
     }
+    
+    func getTripsByStation(station: String) async throws -> TripsByStation {
+        print("getTripsByStation")
+        let response = try await client.getTripsByStation(
+            query: .init(
+                apikey: apikey, station: station
+            )
+        )
+        
+        return try response.ok.body.json
+    }
 }
-
 
 func stations() {
     let client = Client(
@@ -75,6 +86,26 @@ func schedule() {
     
     Task {
         let schedule = try await service.getTripsSchedule("c146", "c213")
-        print(schedule)
+
+        print(tripsByStation)
     }
 }
+
+func tripsByStation() {
+    let client = Client(
+        serverURL: try! Servers.server1(),
+        transport: URLSessionTransport()
+    )
+    
+    let service = NearestStationsService(
+        client: client,
+        apikey: "d52e2ffc-ec87-4d27-8247-5c7f412d514e"
+    )
+    
+    Task {
+        let tripsByStation = try await service.getTripsByStation(station: "s9616628")
+        print(tripsByStation)
+    }
+}
+
+
