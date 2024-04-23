e4cba8c672dbad7bc1d7126ad7abc3e563077255
Georgii <neguritsageorgiy@gmail.com>
2024-04-23 11:02:54 +0400

    add files

---
+++ YPTravel/Screens/TrainSchedule/TrainScheduleRow.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct TrainScheduleRow: View {
+    var body: some View {
+       Text("")
+    }
+}
+
+#Preview {
+    TrainScheduleRow()
+}
---
+++ YPTravel/Screens/TrainSchedule/TrainScheduleView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct TrainScheduleView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+#Preview {
+    TrainScheduleView()
+}
